1fa774e815895d605bc5a08561056b5b9f72a5ee
tiem9154 <tiem9154@gmail.com>
2023-09-21 14:21:29 +0900

    ㅆTest1.java 팀장이 작업

--- src/test/Test2.java
+++ src/test/Test2.java
@@ -4,5 +4,6 @@ public class Test2 {
     public static void main(String[] args) {
         System.out.println("팀장이 작성한 코드");
         System.out.println("hello");
+        System.out.println("팀원이 작성한 코드");
     }
 }
